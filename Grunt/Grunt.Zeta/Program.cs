8686f68e42f0a083aa3c0b7075c2e9a1fe8fb904
Noah Surprenant <noah.surprenant@gmail.com>
2023-12-25 00:36:39 -0500

    Use async main in zeta project

--- Grunt/Grunt.Zeta/Program.cs
+++ Grunt/Grunt.Zeta/Program.cs
@@ -12,7 +12,7 @@ namespace OpenSpartan.Grunt.Zeta
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             var clientConfig = ConfigurationReader.ReadConfiguration<ClientConfiguration>("client.json");
             
@@ -40,165 +40,123 @@ namespace OpenSpartan.Grunt.Zeta
                 currentOAuthToken = RequestNewToken(url, manager, clientConfig);
             }
 
-            Task.Run(async () =>
+            ticket = await manager.RequestUserToken(currentOAuthToken.AccessToken);
+            if (ticket == null)
             {
-                ticket = await manager.RequestUserToken(currentOAuthToken.AccessToken);
-                if (ticket == null)
+                // There was a failure to obtain the user token, so likely we need to refresh.
+                currentOAuthToken = await manager.RefreshOAuthToken(
+                    clientConfig.ClientId,
+                    currentOAuthToken.RefreshToken,
+                    clientConfig.RedirectUrl,
+                    clientConfig.ClientSecret);
+
+                if (currentOAuthToken == null)
                 {
-                    // There was a failure to obtain the user token, so likely we need to refresh.
-                    currentOAuthToken = await manager.RefreshOAuthToken(
-                        clientConfig.ClientId,
-                        currentOAuthToken.RefreshToken,
-                        clientConfig.RedirectUrl,
-                        clientConfig.ClientSecret);
-
-                    if (currentOAuthToken == null)
-                    {
-                        Console.WriteLine("Could not get the token even with the refresh token.");
-                        currentOAuthToken = RequestNewToken(url, manager, clientConfig);
-                    }
-                    ticket = await manager.RequestUserToken(currentOAuthToken.AccessToken);
+                    Console.WriteLine("Could not get the token even with the refresh token.");
+                    currentOAuthToken = RequestNewToken(url, manager, clientConfig);
                 }
-            }).GetAwaiter().GetResult();
+                ticket = await manager.RequestUserToken(currentOAuthToken.AccessToken);
+            }
 
-            Task.Run(async () =>
-            {
-                haloTicket = await manager.RequestXstsToken(ticket.Token);
-            }).GetAwaiter().GetResult();
+            haloTicket = await manager.RequestXstsToken(ticket.Token);
 
-            Task.Run(async () =>
-            {
-                extendedTicket = await manager.RequestXstsToken(ticket.Token, false);
-            }).GetAwaiter().GetResult();
+            extendedTicket = await manager.RequestXstsToken(ticket.Token, false);
 
-            Task.Run(async () =>
-            {
-                haloToken = await haloAuthClient.GetSpartanToken(haloTicket.Token);
-                Console.WriteLine("Your Halo token:");
-                Console.WriteLine(haloToken.Token);
-            }).GetAwaiter().GetResult();
+            haloToken = await haloAuthClient.GetSpartanToken(haloTicket.Token);
+            Console.WriteLine("Your Halo token:");
+            Console.WriteLine(haloToken.Token);
             
             HaloInfiniteClient client = new(haloToken.Token, extendedTicket.DisplayClaims.Xui[0].Xid);
 
             // Test getting the clearance for local execution.
             string localClearance = string.Empty;
-            Task.Run(async () =>
+            var clearance = (await client.SettingsGetClearance("RETAIL", "UNUSED", "222249.22.06.08.1730-0")).Result;
+            if (clearance != null)
             {
-                var clearance = (await client.SettingsGetClearance("RETAIL", "UNUSED", "222249.22.06.08.1730-0")).Result;
-                if (clearance != null)
-                {
-                    localClearance = clearance.FlightConfigurationId;
-                    client.ClearanceToken = localClearance;
-                    Console.WriteLine($"Your clearance is {localClearance} and it's set in the client.");
-                }
-                else
-                {
-                    Console.WriteLine("Could not obtain the clearance.");
-                }
-            }).GetAwaiter().GetResult();
+                localClearance = clearance.FlightConfigurationId;
+                client.ClearanceToken = localClearance;
+                Console.WriteLine($"Your clearance is {localClearance} and it's set in the client.");
+            }
+            else
+            {
+                Console.WriteLine("Could not obtain the clearance.");
+            }
 
             // Try getting actual Halo Infinite data.
-            Task.Run(async () =>
-            {
-                var example = await client.StatsGetMatchStats("21416434-4717-4966-9902-af7097469f74");
-                Console.WriteLine("You have stats.");
-            }).GetAwaiter().GetResult();
+            var example = await client.StatsGetMatchStats("21416434-4717-4966-9902-af7097469f74");
+            Console.WriteLine("You have stats.");
 
-            Task.Run(async () =>
-            {
-                var academyData = await client.AcademyGetContent();
-                Console.WriteLine("Got academy data.");
-            }).GetAwaiter().GetResult();           
+            var academyData = await client.AcademyGetContent();
+            Console.WriteLine("Got academy data.");         
 
             // Test getting the season data.
-            Task.Run(async () =>
-            {
-                var seasonData = await client.GameCmsGetSeasonRewardTrack(
-                    "Seasons/Season7.json",
-                    localClearance);
-                Console.WriteLine("Got season data.");
-            }).GetAwaiter().GetResult();
+            var seasonData = await client.GameCmsGetSeasonRewardTrack(
+                "Seasons/Season7.json",
+                localClearance);
+            Console.WriteLine("Got season data.");
 
             // Try getting skill qualifications with SkillGetMatchPlayerResult.
-            Task.Run(async () =>
-            {
-                List<string> sampleXuids = "xuid(2533274793272155),xuid(2533274814715980),xuid(2533274855333605),xuid(2535435749594170),xuid(2535448099228893),xuid(2535457135464780),xuid(2535466738529606),xuid(2535472868898775)".Split(',').ToList();
-                var performanceData = (await client.SkillGetMatchPlayerResult("ad6a3d46-9320-44ee-94cd-c5cb39c7aedd", sampleXuids)).Result;
-                Console.WriteLine("Got player performance data.");
-            }).GetAwaiter().GetResult();
+            List<string> sampleXuids = "xuid(2533274793272155),xuid(2533274814715980),xuid(2533274855333605),xuid(2535435749594170),xuid(2535448099228893),xuid(2535457135464780),xuid(2535466738529606),xuid(2535472868898775)".Split(',').ToList();
+            var performanceData = (await client.SkillGetMatchPlayerResult("ad6a3d46-9320-44ee-94cd-c5cb39c7aedd", sampleXuids)).Result;
+            Console.WriteLine("Got player performance data.");
 
             // Get an example image and store it locally.
-            Task.Run(async () =>
+            var imageData = (await client.GameCmsGetImage("progression/inventory/armor/gloves/003-001-olympus-8e7c9dff-sm.png")).Result;
+            Console.WriteLine("Got image data.");
+            if (imageData != null)
             {
-                var imageData = (await client.GameCmsGetImage("progression/inventory/armor/gloves/003-001-olympus-8e7c9dff-sm.png")).Result;
-                Console.WriteLine("Got image data.");
-                if (imageData != null)
-                {
-                    System.IO.File.WriteAllBytes("image.png", imageData);
-                    Console.WriteLine("Wrote sample image to file.");
-                }
-                else
-                {
-                    Console.WriteLine("Image could not be written. There was an error.");
-                }
-            }).GetAwaiter().GetResult();
+                System.IO.File.WriteAllBytes("image.png", imageData);
+                Console.WriteLine("Wrote sample image to file.");
+            }
+            else
+            {
+                Console.WriteLine("Image could not be written. There was an error.");
+            }
 
             // Get bot customization data
-            Task.Run(async () =>
+            var botCustomization = (await client.AcademyGetBotCustomization(localClearance)).Result;
+            if (botCustomization != null)
             {
-                var seasonData = (await client.AcademyGetBotCustomization(localClearance)).Result;
-                if (seasonData != null)
-                {
-                    Console.WriteLine("Got but customization data.");
-                }
-                else
-                {
-                    Console.WriteLine("Could not get bot customization data.");
-                }
-            }).GetAwaiter().GetResult();
+                Console.WriteLine("Got but customization data.");
+            }
+            else
+            {
+                Console.WriteLine("Could not get bot customization data.");
+            }
 
             // Get currency data
-            Task.Run(async () =>
+            var currencyData = (await client.GameCmsGetCurrency("currency/currencies/cr.json", localClearance)).Result;
+            if (currencyData != null)
             {
-                var currencyData = (await client.GameCmsGetCurrency("currency/currencies/cr.json", localClearance)).Result;
-                if (currencyData != null)
-                {
-                    Console.WriteLine("Got currency data.");
-                }
-                else
-                {
-                    Console.WriteLine("Could not get currency data.");
-                }
-            }).GetAwaiter().GetResult();
+                Console.WriteLine("Got currency data.");
+            }
+            else
+            {
+                Console.WriteLine("Could not get currency data.");
+            }
 
             // Get reward data.
-            Task.Run(async () =>
+            var rewardData = (await client.EconomyGetAwardedRewards("xuid(2533274855333605)", "Challenges-35a86ae3-017c-4b5a-b633-b2802a770e0a")).Result;
+            if (rewardData != null)
             {
-                var rewardData = (await client.EconomyGetAwardedRewards("xuid(2533274855333605)", "Challenges-35a86ae3-017c-4b5a-b633-b2802a770e0a")).Result;
-                if (rewardData != null)
-                {
-                    Console.WriteLine("Got reward data.");
-                }
-                else
-                {
-                    Console.WriteLine("Could not get reward data.");
-                }
-            }).GetAwaiter().GetResult();
+                Console.WriteLine("Got reward data.");
+            }
+            else
+            {
+                Console.WriteLine("Could not get reward data.");
+            }
 
-            Task.Run(async () =>
-            {
-                var matchData = await client.StatsGetMatchHistory("xuid(2533274855333605)", 0, 12, Models.HaloInfinite.MatchType.All);
+            var matchData = await client.StatsGetMatchHistory("xuid(2533274855333605)", 0, 12, Models.HaloInfinite.MatchType.All);
                 
-                var data = matchData.Result;
-                if (data != null)
-                {
-                    Console.WriteLine("Got reward data.");
-                }
-                else
-                {
-                    Console.WriteLine("Could not get reward data.");
-                }
-            }).GetAwaiter().GetResult();
+            var data = matchData.Result;
+            if (data != null)
+            {
+                Console.WriteLine("Got match history data.");
+            }
+            else
+            {
+                Console.WriteLine("Could not get match history data.");
+            }
 
             Console.ReadLine();
         }
